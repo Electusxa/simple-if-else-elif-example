c7642134a4f929bf0e410bfbeb20fbcef4cd0d47
Ali ElectusXa Polat <81361547+Electusxa@users.noreply.github.com>
2022-05-01 02:23:06 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+age=int(input('Enter age : '))
+if age>=18:
+    print('can get a driver s license')
+else:
+    print('can t get a driver s license')
+
+
+school_name=input('Enter your school name : ')
+
+if school_name=='HASMTAL':
+    print('best schooll')
+else:
+    print('regular school')
+
+
+num1=int(input('Enter a number 1 : '))
+num2=int(input('Enter a number 2 : '))
+
+if num1==num2:
+    print('numbers are equal')
+elif num1<num2:
+    print(num2, '>', num1)
+elif num1>num2:
+    print(num1, '>', num2)
